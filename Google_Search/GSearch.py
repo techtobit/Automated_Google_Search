33c9efafe6bd754a27546f898008c094f39d897a
techtobit <nurthern2016@gmail.com>
2024-12-14 23:48:12 +0600

    removed selenium code, removed issue : " > ...."

--- Google_Search/GSearch.py
+++ Google_Search/GSearch.py
@@ -6,25 +6,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.common.by import By
 import time
+import re
 
-options=Options()
-options.headless=True
+
 
 class Search:
 	def __init__(self):
-		# self.browser =  webdriver.Chrome(options=options)
 
 		# Use Helium 
 		self.browser =  start_chrome(headless=True)
 
 	def gSarch(self,keyword):
-		# self.browser.get('http://www.google.com')
-		# search = self.browser.find_element(By.NAME, 'q')
-		# search.send_keys(keyword)
-		# search.send_keys(Keys.RETURN)
-		# time.sleep(3)
-
-		# Use Helium 
 		go_to("https://www.google.com")
 		search_box = find_all(S("textarea[name='q']"))[0]
 		write('python', into=search_box)
@@ -32,22 +24,19 @@ class Search:
 		time.sleep(3)
 
 	def getResult(self):
-		# results=WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//cite")))
-		# urls= [result.text for result in results if result.text.strip()]
-		
-		# Use Helium 
-		# wait_until(lambda: len(find_all(S("cite"))) >0, timeout_secs=10)
-		# elements=find_all(S("cite"))
-		# urls=[element.web_element.text for element in elements if element.web_element.text.strip()]
-
-
 		wait_until(lambda: len(find_all(S("cite"))) > 0, timeout_secs=10)
 		elements = find_all(S("cite"))
-		urls = [element.web_element.text for element in elements if element.web_element.text.strip()]
+		urls = []
+
+		for element in elements:
+			text=element.web_element.text.strip()
+			match = re.match(r'https?://\S+', text)
+			if match:
+					urls.append(match.group(0)) 
 		print(urls)
 
 	def closeBrowser(self):
-		# self.browser.close()
 		kill_browser()
+
 
 
